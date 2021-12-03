6056a129a67280ebe9cc2f6585fd85edf850b72d
Manuel Naranjo <naranjo.manuel@gmail.com>
2021-12-03 10:33:09 +0100

    [scripts] cleaning up rpm2cpio
    Now that rpm2cpio works it's time to clean it up

--- rpm2cpio.py
+++ rpm2cpio.py
@@ -1,17 +1,12 @@
 import sys
 
 from pyrpm.rpm import RPM
-from pyrpm import rpmdefs
-
 
 def main(file_name):
     rpm = RPM(open(file_name, 'rb'))
-    #print('is binary:', rpm.binary)
-    #print('is source:', rpm.source)
     for tag, value in rpm.items().items():
         print(f'{tag} -> {value}', file=sys.stderr)
     sys.stdout.buffer.write(rpm.get_payload())
-    #print(rpm.payload)
 
 
 if __name__ == '__main__':
